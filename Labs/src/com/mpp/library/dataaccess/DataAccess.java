7b166070c043d3d93cb7cc21f2a7a931b8ac3b37
thuyhangha <hthang84@gmail.com>
2016-09-14 22:47:18 +0700

    get all map

--- Labs/src/com/mpp/library/dataaccess/DataAccess.java
+++ Labs/src/com/mpp/library/dataaccess/DataAccess.java
@@ -1,8 +1,11 @@
 package com.mpp.library.dataaccess;
 
+import java.util.Map;
+
 public interface DataAccess<K, V> {
 	public V add(K key, V value);
 	public V update(K key, V value);
 	public V get(K key);
 	public V delete(K key);
+	public Map<K, V> getAll();
 }
