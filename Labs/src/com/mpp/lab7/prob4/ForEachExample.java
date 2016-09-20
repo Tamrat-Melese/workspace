b4739d2a2a9ad55902c06f24dc7bbb311491e0b6
Tamrat-Melese <tamratmel@gmail.com>
2016-09-20 18:00:52 -0500

    modify problem4 solution

--- Labs/src/com/mpp/lab7/prob4/ForEachExample.java
+++ Labs/src/com/mpp/lab7/prob4/ForEachExample.java
@@ -2,22 +2,12 @@ package com.mpp.lab7.prob4;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ForEachExample {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation",
 				"Everywhere you want to be");
 		
-		Consumer<String> consumer = ForEachExample::printInUpperCase;
-		
-		for (String s : list) {
-			consumer.accept(s.toUpperCase());
-		}
-
-	}
-
-	public static void printInUpperCase(String str) {
-		System.out.println(str);
+		list.forEach(s -> System.out.println(s.toUpperCase()));
 	}
 }
