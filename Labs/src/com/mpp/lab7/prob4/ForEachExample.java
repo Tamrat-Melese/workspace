b3ab503921eeab55bd0a37aee7d1f9d3612a0a63
Tamrat-Melese <tamratmel@gmail.com>
2016-09-20 21:42:42 -0500

    enhance prob4 solution

--- Labs/src/com/mpp/lab7/prob4/ForEachExample.java
+++ Labs/src/com/mpp/lab7/prob4/ForEachExample.java
@@ -2,12 +2,25 @@ package com.mpp.lab7.prob4;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ForEachExample {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation",
 				"Everywhere you want to be");
 		
-		list.forEach(s -> System.out.println(s.toUpperCase()));
+		Consumer<String> consumer = toUpperCase();
+		list.forEach(consumer);
+
+	}
+
+	private static Consumer<String> toUpperCase() {
+		Consumer<String> consumer = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s.toUpperCase());
+			}
+		};
+		return consumer;
 	}
 }
