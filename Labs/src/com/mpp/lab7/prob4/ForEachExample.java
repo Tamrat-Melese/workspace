a93f5295652e8951fcdbb2cce5b193e7a5f7d200
An Nguyen <anthony@Anthonys-MacBook-Pro.local>
2016-09-20 21:43:11 -0500

    no message

--- Labs/src/com/mpp/lab7/prob4/ForEachExample.java
+++ Labs/src/com/mpp/lab7/prob4/ForEachExample.java
@@ -7,7 +7,7 @@ public class ForEachExample {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation",
 				"Everywhere you want to be");
-		
+
 		list.forEach(s -> System.out.println(s.toUpperCase()));
 	}
 }
