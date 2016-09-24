510943b6a4d596ce87526ac3c4c0fb41e08b25d4
nguyenhongan109 <nguyenhongan109@gmail.com>
2016-09-24 13:21:21 -0500

    Prob6

---
+++ Labs/src/com/mpp/lab9/prob6/LambdaLibrary.java
@@ -0,0 +1,19 @@
+package com.mpp.lab9.prob6;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LambdaLibrary {
+    public static Predicate<Employee> salaryGreaterThan(int i){
+        return e -> e.getSalary() > i;
+    }
+
+    public static Predicate<Employee> fistLeterInLastNameComeAfter(char c){
+        return e -> e.getLastName().charAt(0) > c;
+    }
+
+    public static Function<Employee, String> fullNameInCapital(){
+        return e -> (e.firstName + " " + e.getLastName()).toUpperCase();
+    }
+}
