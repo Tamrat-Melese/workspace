ddf8dd1cadb78c235522e4e47759660f8689a25b
Tamrat-Melese <tamratmel@gmail.com>
2016-09-23 21:29:31 -0500

    refactor package name

---
+++ Labs/src/com/mpp/lab9/prob7/Main.java
@@ -0,0 +1,16 @@
+package com.mpp.lab9.prob7;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Book book = new Book("test", 3);
+		List<BookCopy> copies = book.getCopies();
+		copies.forEach(copy -> copy.changeAvailability());
+		
+		//test
+		System.out.println(book.isAvailable());
+	}
+
+}
